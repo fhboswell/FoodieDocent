83ffdf522208698918965a1bb9e5ec88cb9df73b
Henry <franklin.henry.boswell@gmail.com>
2015-04-28 16:42:24 -0700

    add immages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -70,6 +70,6 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:description)
+      params.require(:place).permit(:description, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428010806) do
+ActiveRecord::Schema.define(version: 20150428231907) do
 
   create_table "places", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "places", ["user_id"], name: "index_places_on_user_id"
