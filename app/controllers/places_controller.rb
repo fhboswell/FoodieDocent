c8137ac53d9e4ebf165c90c39d9991db78b591c7
Henry <franklin.henry.boswell@gmail.com>
2015-04-28 16:02:42 -0700

    user permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,8 @@
 
 class PlacesController < ApplicationController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def index
@@ -15,35 +17,33 @@ class PlacesController < ApplicationController
 
  
   def new
-    @place = Place.new
+    @place = current_user.places.build
   end
 
   def edit
   end
 
   def create
-    @place = Place.new(place_params)
+    @place = current_user.places.build(place_params)
 
-    respond_to do |format|
-      if @place.save
-        format.html { redirect_to @place, notice: 'Place was successfully created.' }
-        format.json { render :show, status: :created, location: @place }
-      else
-        format.html { render :new }
-        format.json { render json: @place.errors, status: :unprocessable_entity }
-      end
+    
+    if @place.save
+      redirect_to @place, notice: 'Place was successfully created.'
+      
+    else
+      render action: 'new'
+      
     end
+   
   end
 
   def update
     
-      if @place.update(place_params)
-      redirect_to @place, notice: 'Place was successfully updated.' 
-        
-      else
-        render action 'edit' 
-       
-     
+    if @place.update(place_params)
+    redirect_to @place, notice: 'Place was successfully updated.' 
+      
+    else
+      render action: 'edit' 
     end
   end
 
@@ -61,6 +61,11 @@ class PlacesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_place
       @place = Place.find(params[:id])
+    end
+
+    def correct_user
+      @place = current_user.places.find_by(id: params[:id])
+      redirect_to places_path, notice: "Cant Edit" if @place.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
